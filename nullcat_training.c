db9c400239d2c66b2dc6b6334812bf83cbb60f1f
basilep <basile.perreaux@gmail.com>
2023-05-08 17:48:06 +0200

    struct neighboor instead of hardcoded adresses

--- nullcat_training.c
+++ nullcat_training.c
@@ -5,16 +5,28 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "sys/node-id.h"
+
 /* LOG CONFIGURATION */
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-/* CONFIGURATION */
+/* OTHER CONFIGURATION */
 #define SEND_INTERVAL (500 * CLOCK_SECOND)
+
 static linkaddr_t null_addr = {{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};//TOCHANGE
-static linkaddr_t first_addr = {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
-static linkaddr_t second_addr  = {{ 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
+
+static unsigned data_to_send = 10;  //TOCHANGE
+
+static int in_network = 0; // Says if the node is already connected to the network ()
+
+typedef struct neighboor
+{
+  linkaddr_t neighboor_addr;
+}neighboor;
+
+static neighboor neighboor_table;
 
 /* PROCESS CREATION */
 PROCESS(node_example, "Node Example");
@@ -27,9 +39,25 @@ void input_callback(const void *data, uint16_t len,
   if(len == sizeof(unsigned)) {
     unsigned data_receive;
     memcpy(&data_receive, data, sizeof(data_receive));
-    LOG_INFO("Received %u from ", data_receive);
-    LOG_INFO_LLADDR(src);
-    LOG_INFO_("\n");
+    if(data_receive == 0){
+      LOG_INFO("Received a connection request from ");
+      LOG_INFO_LLADDR(src);
+      LOG_INFO_("\n");
+      data_to_send   = 1;
+      NETSTACK_NETWORK.output(src);
+      linkaddr_copy(&(neighboor_table.neighboor_addr), src);
+      //memcpy(&(routing_table.routes[routing_table.nb_route].next_hop_addr), src, sizeof(linkaddr_t));
+      in_network = 1;
+    }
+    else if(data_receive == 1){
+      LOG_INFO_LLADDR(src);
+      LOG_INFO_(" has accpeted my connection request\n");
+      linkaddr_copy(&(neighboor_table.neighboor_addr), src);
+      in_network = 1;
+      }
+    else{
+      LOG_INFO("We are already connected, you send me %u\n", data_receive);
+    }    
   }
 }
 
@@ -38,7 +66,6 @@ PROCESS_THREAD(node_example, ev, data)
 {
   // Declare the timer
   static struct etimer timer;
-  static unsigned data_to_send = 0;
 
   PROCESS_BEGIN();
 
@@ -52,22 +79,22 @@ PROCESS_THREAD(node_example, ev, data)
     etimer_set(&timer, SEND_INTERVAL);
     while(1) {
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+
+
+    if(!in_network){
+      // Not in the network at the moment -> broadcast a packet to know the neighboors
+      LOG_INFO("Hi, I\'m the node %u would like to join the network, let's broadcast the signal 0\n", node_id); //node_id return the ID of the current node
+      data_to_send = 0;
+      NETSTACK_NETWORK.output(NULL);
+    }
+
+    else{
       LOG_INFO("My data to send is %u\n", data_to_send);
-      //LOG_INFO_LLADDR(&dest_addr);
-      //LOG_INFO_("\n");
-      if(linkaddr_cmp(&first_addr, &linkaddr_node_addr)){
-        NETSTACK_NETWORK.output(&second_addr);  // Use to sent data to the destination
-        data_to_send++;
-      }
-      else if(linkaddr_cmp(&second_addr, &linkaddr_node_addr)){
-        NETSTACK_NETWORK.output(&first_addr);  // Use to sent data to the destination
-        data_to_send=data_to_send+2;
-      }
-      else{
-        LOG_INFO("I\'m not first or second node, but lets broadcast 0 to annoy everyone\n");
-        NETSTACK_NETWORK.output(NULL);
-      }
-      etimer_reset(&timer);
+      NETSTACK_NETWORK.output(&(neighboor_table.neighboor_addr));  // Use to sent data to the destination
+      data_to_send++;
+
+    }
+    etimer_reset(&timer);
     }
   }
   PROCESS_END();
